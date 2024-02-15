3c3db9f762a2fdae8da5153161c83ea47a7eb3c2
David Conner <aionfork@gmail.com>
2024-02-15 09:33:35 -0500

    add metadata and correct formatting in package.json

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Make Roanoke";
+export const SITE_TITLE = "MAKE Roanoke";
 export const SITE_DESCRIPTION = "Welcome to my website!";
