eb9962e04c05bbc13997bc0ec11f37e7eca75907
David Conner <aionfork@gmail.com>
2024-02-15 13:24:12 -0500

    Add fira sans as the main font

--- src/consts.ts
+++ src/consts.ts
@@ -7,3 +7,16 @@ export const SITE_DESCRIPTION = "Welcome to my website!";
 export const COPYRIGHT = "MAKE Roanoke";
 
 export const REMOTE_ASSETS_BASE_URL = `${import.meta.env.SITE}${import.meta.env.BASE_URL}`;
+
+export interface GoogleFont {
+  name: string;
+  weights?: string;
+}
+
+const googleFonts = [
+  { name: "Fira Sans", weights: "400,400i,600,600i"},
+  { name: "Istok Web", weights: "400,400i,600,600i"}];
+
+export const theme = {
+  googleFonts
+};
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,55 @@ export default {
     './node_modules/flowbite/**/*.js'
   ],
 	theme: {
-		extend: {},
+		extend: {
+
+			fontFamily: {
+				sans: [
+          'Fira Sans',
+					'ui-sans-serif',
+					'system-ui',
+					'-apple-system',
+					'system-ui',
+					'Segoe UI',
+					'Roboto',
+					'Helvetica Neue',
+					'Arial',
+					'Noto Sans',
+					'sans-serif',
+					'Apple Color Emoji',
+					'Segoe UI Emoji',
+					'Segoe UI Symbol',
+					'Noto Color Emoji',
+				],
+				body: [
+          'Fira Sans',
+					'ui-sans-serif',
+					'system-ui',
+					'-apple-system',
+					'system-ui',
+					'Segoe UI',
+					'Roboto',
+					'Helvetica Neue',
+					'Arial',
+					'Noto Sans',
+					'sans-serif',
+					'Apple Color Emoji',
+					'Segoe UI Emoji',
+					'Segoe UI Symbol',
+					'Noto Color Emoji',
+				],
+				mono: [
+					'ui-monospace',
+					'SFMono-Regular',
+					'Menlo',
+					'Monaco',
+					'Consolas',
+					'Liberation Mono',
+					'Courier New',
+					'monospace',
+				],
+      }
+    },
 	},
 	plugins: [
     require('flowbite/plugin'),
