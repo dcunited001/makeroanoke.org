6fd9d47deee266db73d1d7a9ac29265e2786328a
David Conner <aionfork@gmail.com>
2024-02-16 07:36:08 -0500

    update editorconfig, prettierrc

---
+++ .prettierrc.mjs
@@ -0,0 +1,17 @@
+// npm install --save-dev prettier prettier-plugin-astro
+
+/** @type {import("prettier").Config} */
+export default {
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+};
