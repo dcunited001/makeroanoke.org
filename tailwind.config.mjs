788f67e457d43d27c551c7960838e250ca5b3569
David Conner <aionfork@gmail.com>
2024-02-24 16:09:07 -0500

    CSS Color Variables Hack in Tailwind FTW

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,6 +10,33 @@ export default {
       transitionProperty: {
         width: 'width'
       },
+      colors: {
+        accent: {
+          1: "hsl(288 95.8% 90.6%)",
+          2: "hsl(168 83.8% 78.2%)",
+        },
+        bkg: "hsl(210 40% 98%)",
+        content: "hsl(217 32.6% 17.5%)",
+
+        // NOTE: use with bg-navy, etc
+        white: 'hsl(var(--color-white))',
+        navy: 'hsl(var(--color-navy))',
+        wood: 'hsl(var(--color-wood))',
+        metal: 'hsl(var(--color-metal))',
+        fabric: 'hsl(var(--color-fabric))',
+        tronics: 'hsl(var(--color-tronics))',
+        printing: 'hsl(var(--color-printing))',
+
+        // TODO: set a default for alpha value
+        // white: 'hsl(var(--color-white)) / <alpha-value>',
+        // navy: 'hsl(var(--color-navy)) / <alpha-value>',
+        // wood: 'hsl(var(--color-wood)) / <alpha-value>',
+        // metal: 'hsl(var(--color-metal)) / <alpha-value>',
+        // fabric: 'hsl(var(--color-fabric)) / <alpha-value>',
+        // tronics: 'hsl(var(--color-tronics)) / <alpha-value>',
+        // printing: 'hsl(var(--color-printing)) / <alpha-value>'
+      },
+
 			fontFamily: {
 				sans: [
           'Fira Sans',
@@ -62,5 +89,41 @@ export default {
     require('flowbite/plugin'),
     require('flowbite-typography'),
     require('tailwind-scrollbar')({ nocompatible: true }),
+
+    // fetching dark mode from user's preferences
+    // https://www.youtube.com/watch?v=WTchW0LdWL0
+    function ({ addComponents, theme }) {
+      addComponents({
+        ':root': {
+          '--color-bkg': '210deg 40% 98%',
+          '--color-content': '217deg 32.6% 17.5%',
+          '--color-accent1': '288deg 95.8% 90.6%',
+          '--color-accent2': '186deg 83.8% 78.2%',
+          '--color-white': '0deg 0% 100%',
+          '--color-navy': '222deg 75% 15%',
+          '--color-wood': '24deg 100% 55%',
+          '--color-metal': '220deg 94% 51%',
+          '--color-fabric': '189deg 76% 53%',
+          '--color-tronics': '298deg 62% 47%',
+          '--color-printing': '99deg 72% 59%'
+        },
+        // TODO: set color variables for dark theme
+        '@media (prefers-color-scheme:dark)': {
+          ':root': {
+            '--color-bkg': '217deg 32.6% 17.5%',
+            '--color-content': '210deg 40% 98%',
+            '--color-accent1': '288deg 95.8% 60.6%',
+            '--color-accent2': '186deg 83.8% 48.2%',
+            '--color-white': '0deg 0% 100%',
+            '--color-navy': '222deg 75% 15%',
+            '--color-wood': '24deg 100% 55%',
+            '--color-metal': '220deg 94% 51%',
+            '--color-fabric': '189deg 76% 53%',
+            '--color-tronics': '298deg 62% 47%',
+            '--color-printing': '99deg 72% 59%'
+          },
+        }
+      })
+    }
   ],
 }
